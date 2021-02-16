74165c24a3b6e7a0e343663f4ea0e3c6153724a5
LiamBartolini <liambartolini@gmail.com>
2021-02-16 23:34:52 +0100

    close #1

--- js/script.js
+++ js/script.js
@@ -2,11 +2,11 @@
 
 var indiceNominativi = 0;
 var indiceInseriti = 0;
-var isValid;
-var vale = false;
 var nominativi = [];
 var inseriti = [];
 var blocco;
+var firstClick = false;
+var nome, cognome;
 
 jQuery(function () {
     // blocco il pulsante per evitare inserimenti sbagliati
@@ -14,76 +14,81 @@ jQuery(function () {
     jQuery("#txtNome , #txtCognome").on("focus", function () {
         jQuery("#btnInserisci").removeAttr("disabled"); // quando uno dei due campi input viene focussato allora il pulsante si sblocca
     });
-        // controllo se è stato premuto il pulsante
+    // controllo se è stato premuto il pulsante
     jQuery("#btnInserisci").on("click", function() {
-        const nome = document.getElementById("txtNome").value;
-        const cognome = document.getElementById("txtCognome").value;
-        verify(nome, cognome);
+        nome = document.getElementById("txtNome").value;
+        cognome = document.getElementById("txtCognome").value;
+
+        if (nome.length >= 2 && cognome.length >= 2) {
+            if (checkString(nome) && checkString(cognome)) {
+                activeModalConferma("Confermare l'inserimento");
+            } else {
+                activeModalError("il nome/cognome <b>'" + nome + " " + cognome + "'</b> contiene un numero!");
+            }
+        } else {
+            activeModalError("Lunghezza <b>'" + nome + " " + cognome + "'</b> troppo corta");
+        }
+
         // pulisco i campi di input
         document.getElementById("txtNome").value = "";
         document.getElementById("txtCognome").value = "";
-        if (indiceNominativi > 0) {
-            if (nominativi[indiceNominativi - 2] != nominativi[indiceNominativi - 1]) {
-                blocco = jQuery("<p>" + nominativi[indiceNominativi - 1] + "</p>");
-                blocco.addClass("inseriti");
-                blocco.appendTo("#div7");
-            } else {
-                blocco = jQuery("<p>inputError</p>");
-                blocco.addClass("inseriti");
-                blocco.appendTo("#div7");
-            }
-        }
+
         // tolgo l'attributo disabled per la visualizzazione della list group
         jQuery("#btnListGroup").removeAttr("disabled");
     });
     
+    jQuery("#btnAccetta").on("click", function () { 
+        // se accetta inserisco il nominativo
+        if (verifyFirstChar(nome, cognome)) {
+            // se la prima è una vocale allora la aggiungo ai nominativi
+            nominativi[indiceNominativi] = nome + " " + cognome;
+            indiceNominativi++;
+            blocco = jQuery("<p>" + nominativi[indiceNominativi - 1] + "</p>");
+            blocco.addClass("inseriti");
+            blocco.appendTo("#div7");
+        }
+
+        // salvo il nome e cognome come oggetto
+        var daSalvare = {"nome" : nome, "cognome" : cognome};
+        inseriti[indiceInseriti] = daSalvare;
+        indiceInseriti++;
+    });
     
     jQuery("#btnListGroup").on("click", function() {
-        if (indiceInseriti != 0) {
-            for (let valore of inseriti) {
-                blocco = jQuery("<li>" + valore.nome + " " + valore.cognome + "</li>")
+        // controllo se è la prima volta che premo il pulsante per vedere la list gruop
+        if (firstClick) {
+            blocco = jQuery("<li>" + inseriti[indiceInseriti - 1].nome + " " + inseriti[indiceInseriti - 1].cognome + "</li>")
+            blocco.addClass("listElement");
+            blocco.appendTo("#list");    
+        } else {
+            for (let i = 0; i < inseriti.length; i++) {
+                blocco = jQuery("<li>" + inseriti[i].nome + " " + inseriti[i].cognome + "</li>")
                 blocco.addClass("listElement");
                 blocco.appendTo("#list");
             }
+            firstClick = true;
         }
+
         jQuery("#list").removeAttr("hidden");
         jQuery("#btnListGroup").prop("disabled", true);
     });
 });
 
-
-function verify(nome, cognome) {
+function verifyFirstChar(nome, cognome) {
     var vocali = ['A', 'E', 'I', 'O', 'U'];
 
-    // controllo se la lunghezza è maggiore di 2
-    if (nome.length >= 2 && cognome.length >= 2) {
-        // controllo se entrambi i nomi sono validi
-        if (checkValue(nome) && checkValue(cognome)) {
-            for (let i = 0; i < vocali.length; i++) {
-                // controllo che la prima lettera sia una vocale
-                if (nome[0].toUpperCase() == vocali[i] || cognome[0].toUpperCase() == vocali[i]) {
-                    vale = true;
-                    break;
-                } else {
-                    vale = false;
-                }
-            }
-            // se la prima è una vocale allora la aggiungo ai nominativi
-            if (vale) {
-                nominativi[indiceNominativi] = nome + " " + cognome;
-                indiceNominativi++;
-            }
-            // creo un oggetto e lo inserisco dentro l'array di oggetti
-            var daSalvare = {"nome" : nome, "cognome" : cognome};
-            inseriti[indiceInseriti] = daSalvare;
-            indiceInseriti++;
-        } else {
-            activeModalError("il nome <b>'" + arguments[0] + "'</b> contiene un numero!");
+    for (let i = 0; i < vocali.length; i++) {
+        // controllo che la prima lettera sia una vocale
+        if (nome[0].toUpperCase() == vocali[i] || cognome[0].toUpperCase() == vocali[i]) {
+            return true;
         }
-    } else {
-        // in caso apro un modale che dice del nome troppo corto
-        activeModalError("Lunghezza <b>'" + nome + " " + cognome + "'</b> troppo corta");
     }
+    return false;
+}
+
+function activeModalConferma(testo) {
+    jQuery("#testo-modal").html(testo);
+    jQuery("#modal-conferma").modal("toggle");
 }
 
 function activeModalError(testo) {
@@ -91,7 +96,7 @@ function activeModalError(testo) {
     jQuery("#error-modal").modal("toggle");
 }
 
-function checkValue() {
+function checkString() {
     // controlla se tutto il nome/cognome siano senza numeri o caratteri particolari
     for (let i = 0; i < arguments[0].length; i++) {
         let valore = arguments[0].charCodeAt(i);
